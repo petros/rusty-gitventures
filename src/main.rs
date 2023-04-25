f1fd22681e1eaee062b6225877d9af6548407fc9
Petros Amoiridis <petros@amiridis.net>
2023-04-25 17:33:02 +0300

    Open repo and list remotes

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,26 @@ use std::path::PathBuf;
 mod repository;
 
 fn main() {
-    let path = PathBuf::from("~/Projects/petros/rusty-gitventures");
-    let repository = GitRepository::new(path);
-    println!("Repository path: {}", repository.path.to_string_lossy());
+    let path = PathBuf::from("/Users/petros/Projects/petros/rusty-gitventures");
+    println!("Playing with git2");
+    println!("-----------------");
+    println!("");
+
+    println!("Creating repository...");
+    match GitRepository::new(path) {
+        Ok(git_repo) => {
+            println!(
+                "Repository opened successfully with a path of {}!",
+                git_repo.path().to_str().unwrap()
+            );
+            println!("Path: {:?}", git_repo.repository_path());
+
+            println!("");
+            println!("List all remotes...");
+            git_repo.list_remotes();
+        }
+        Err(e) => {
+            eprintln!("Error opening repository: {:?}", e);
+        }
+    }
 }
--- src/repository.rs
+++ src/repository.rs
@@ -1,11 +1,33 @@
+use git2::Repository;
 use std::path::PathBuf;
 
 pub struct GitRepository {
-    pub(crate) path: PathBuf,
+    path: PathBuf,
+    repo: Repository,
 }
 
 impl GitRepository {
-    pub fn new(path: PathBuf) -> Self {
-        Self { path }
+    pub fn new(path: PathBuf) -> Result<Self, git2::Error> {
+        let repo = Repository::open(&path)?;
+        Ok(Self { path, repo })
+    }
+
+    pub fn path(&self) -> &PathBuf {
+        &self.path
+    }
+
+    pub fn repository_path(&self) -> String {
+        self.repo.path().to_str().unwrap().to_string()
+    }
+
+    pub fn list_remotes(&self) {
+        let remotes = self.repo.remotes().unwrap();
+        for remote in remotes.iter() {
+            println!("{}", remote.unwrap());
+            match self.repo.find_remote(remote.unwrap()) {
+                Ok(remote) => println!("  {}", remote.url().unwrap()),
+                Err(e) => eprintln!("  Error finding remote information: {:?}", e),
+            }
+        }
     }
 }
